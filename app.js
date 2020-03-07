71510103b122cd94fdfe9f1e0dab395d4786e843
Konstantin Arakantsev <kostya3312@gmail.com>
2020-03-07 16:31:16 +0300

    Setup GraphQL

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const morgan = require("morgan");
 
 const app = express();
+
+app.use(morgan("tiny"));
+
+app.use("/graphql", (req, res) => graphqlHTTP({}));
 
 const PORT = process.env.PORT || 4000;
 
