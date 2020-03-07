21c553a563c3ee017c0b29ead5e2e0b91911b4c8
Konstantin Arakantsev <kostya3312@gmail.com>
2020-03-07 18:59:30 +0300

    Connecting w/ DB

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const morgan = require("morgan");
 const app = express();
-
+const mongoose = require("mongoose");
 const schema = require("./schema/schema");
+
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connection w/ DB established");
+  })
+  .catch(error => {
+    console.log("DB connection error:", error);
+  });
 
 app.use(morgan("tiny"));
 
