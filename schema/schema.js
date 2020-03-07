271f03ba65373150a5efeb7a74d3c77ba1de888e
Konstantin Arakantsev <kostya3312@gmail.com>
2020-03-07 18:19:21 +0300

    Add relation author -> books

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,22 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList
+} = graphql;
 
 const books = [
   { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
   { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" }
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+  { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
+  { name: "The Color of Magic", genre: "Fantasy", id: "5", authorId: "3" },
+  { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" }
 ];
 
 const authors = [
@@ -48,7 +58,13 @@ const AuthorType = new GraphQLObjectType({
       type: GraphQLString
     },
     age: {
-      type: graphql.GraphQLInt
+      type: GraphQLInt
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, {authorId: parent.id})
+      }
     }
   })
 });
